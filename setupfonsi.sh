92a71b5b1bcd3148353c7c202e8e0e5959780ef7
Fons van der Plas <fonsvdplas@gmail.com>
2020-07-06 23:01:21 +0200

    yupupupuyuypuypuypuypupupy

--- setupfonsi.sh
+++ setupfonsi.sh
@@ -14,7 +14,11 @@ wget https://julialang-s3.julialang.org/bin/linux/x64/1.5/$(echo $JULIA_NAME)-li
 tar -xvzf $JULIA_NAME-linux-x86_64.tar.gz
 rm $JULIA_NAME-linux-x86_64.tar.gz
 
-sudo ln -s /home/fons/$JULIA_NAME/bin/julia /usr/bin/julia
+echo ""
+echo "Your password is fons"
+echo ""
+
+sudo ln -fs /home/fons/$JULIA_NAME/bin/julia /usr/bin/julia
 
 julia --version
 
@@ -25,5 +29,5 @@ cd ~
 git clone https://github.com/fonsp/Pluto.jl.git
 git clone https://github.com/fonsp/PlutoUI.jl.git
 
-julia -e "import Pkg; Pkg.dev(path=\"Pluto.jl\"); Pkg.dev(path=\"PlutoUI.jl\"); import Pluto; import PlutoUI"
+julia -e "import Pkg; Pkg.develop(path=\"Pluto.jl\"); Pkg.develop(path=\"PlutoUI.jl\"); import Pluto; import PlutoUI"
 
