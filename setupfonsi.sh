54194685542c56de7695947b0789b3e4f66eacb7
Fons van der Plas <fonsvdplas@gmail.com>
2021-09-24 22:55:23 +0200

    Update setupfonsi.sh

--- setupfonsi.sh
+++ setupfonsi.sh
@@ -8,7 +8,7 @@ touch .hushlogin
 
 cd ~
 
-JULIA_NAME=julia-1.6.0
+JULIA_NAME=julia-1.7.0-rc1
 
 wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/$(echo $JULIA_NAME)-linux-x86_64.tar.gz
 tar -xvzf $JULIA_NAME-linux-x86_64.tar.gz
@@ -22,12 +22,10 @@ sudo ln -fs /home/fons/$JULIA_NAME/bin/julia /usr/bin/julia
 
 julia --version
 
-# clone my WIP
-
 cd ~
 
-git clone https://github.com/fonsp/Pluto.jl.git
-git clone https://github.com/fonsp/PlutoUI.jl.git
+# git clone https://github.com/fonsp/Pluto.jl.git
+# git clone https://github.com/fonsp/PlutoUI.jl.git
 
-julia -e "import Pkg; Pkg.develop(path=\"Pluto.jl\"); Pkg.develop(path=\"PlutoUI.jl\"); import Pluto; import PlutoUI"
+# julia -e "import Pkg; Pkg.develop(path=\"Pluto.jl\"); Pkg.develop(path=\"PlutoUI.jl\"); import Pluto; import PlutoUI"
 
